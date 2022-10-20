e4d574fb8f6f179f72168ca0cbaed4f43cfad83f
CoolGuyGames <51512210+CoolGuyGames@users.noreply.github.com>
2022-10-20 07:47:36 -0400

    Added Initial Game Files

---
+++ Assets/scripts/ItemPickup.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public LayerMask mask;
+    private bool isLooking;
+    private bool isHolding = false;
+    public Camera mainCamera;
+
+    public float castDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    RaycastHit hit;
+    GameObject previousHit = null;
+    void Update()
+    {
+
+        if (!isHolding)
+        {
+            isLooking = Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, castDistance, mask);
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (isLooking)
+            {
+                isHolding = true;
+                previousHit = hit.transform.gameObject;
+            }
+        }
+        else
+        {
+            isHolding = false;
+        }
+
+        
+        if (!isHolding)
+        {
+            if (previousHit != null)
+            {
+                previousHit.GetComponent<Rigidbody>().useGravity = true;
+                previousHit.GetComponent<Rigidbody>().freezeRotation = false;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isHolding)
+        {
+            previousHit.GetComponent<Rigidbody>().useGravity = false;
+            previousHit.GetComponent<Rigidbody>().freezeRotation = true;
+            previousHit.GetComponent<Rigidbody>().MovePosition(previousHit.transform.position + (((mainCamera.transform.position + (mainCamera.transform.forward * (castDistance - 1))) - previousHit.transform.position) * Time.deltaTime * 20));
+            
+        }
+    }
+}
---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float originalSpeed;
+    public float jumpSpeed;
+    public float sprintSpeed;
+
+    public float jumpHeight;
+
+    public Rigidbody playerBody;
+    public GameObject player;
+    public Camera mainCamera;
+
+    public Vector3 direction;
+
+    public GameObject groundCheck;
+    public float checkRadius;
+    public bool isGrounded;
+
+    public LayerMask mask;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerBody.MovePosition(playerBody.position + direction * Time.deltaTime);
+
+    }
+    private void Update()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        direction *= speed;
+        direction = playerBody.rotation * direction;
+
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, checkRadius, mask);
+        if (!isGrounded)
+        {
+            speed = jumpSpeed;
+        }
+        else 
+        {
+            speed = originalSpeed;
+            if (Input.GetKeyDown(KeyCode.Space))
+                playerBody.AddForce(new Vector3(0, jumpHeight, 0));
+        }
+
+    }
+}
