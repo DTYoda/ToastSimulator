e690d12acb1648f0607fa112f8e22413b48cbaff
CoolGuyGames <51512210+CoolGuyGames@users.noreply.github.com>
2022-10-24 08:57:24 -0400

    Trying to fix ItemPickup

--- Assets/scripts/ItemPickup.cs
+++ Assets/scripts/ItemPickup.cs
@@ -21,13 +21,18 @@ public class ItemPickup : MonoBehaviour
     // Update is called once per frame
     RaycastHit hit;
     GameObject previousHit = null;
+    Vector3 cameraPosition;
     void Update()
     {
+
+        cameraPosition = mainCamera.transform.position + (castDistance - 1) * mainCamera.transform.forward;
 
         if (!isHolding)
         {
             isLooking = Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, castDistance, mask);
         }
+
+        Debug.Log(hit.transform.position);
 
         if (Input.GetKey(KeyCode.F))
         {
@@ -43,24 +48,34 @@ public class ItemPickup : MonoBehaviour
         }
 
         
-        if (!isHolding)
+        if (!isHolding && previousHit != null)
         {
-            if (previousHit != null)
-            {
-                previousHit.GetComponent<Rigidbody>().useGravity = true;
-                previousHit.GetComponent<Rigidbody>().freezeRotation = false;
-            }
+            previousHit.GetComponent<Rigidbody>().useGravity = true;
+            previousHit.GetComponent<Rigidbody>().freezeRotation = false;
         }
-    }
-    private void FixedUpdate()
-    {
+
+        Debug.Log(cameraPosition);
+        Debug.Log(previousHit.transform.position);
+        if (previousHit.transform.position != cameraPosition)
+        {
+            Debug.Log("Not equal");
+        }
+        else
+        {
+            Debug.Log("They are equal");
+        }
         if (isHolding)
         {
             previousHit.GetComponent<Rigidbody>().useGravity = false;
             previousHit.GetComponent<Rigidbody>().freezeRotation = true;
-            previousHit.GetComponent<Rigidbody>().MovePosition(previousHit.transform.position + (((mainCamera.transform.position + (mainCamera.transform.forward * (castDistance - 1))) - previousHit.transform.position) * Time.deltaTime * itemSpeed));
+            previousHit.transform.position = cameraPosition;
+
             previousHit.transform.eulerAngles = new Vector3(previousHit.transform.eulerAngles.x, mainCamera.transform.eulerAngles.y, previousHit.transform.eulerAngles.z);
-            
+
         }
+    }
+    private void FixedUpdate()
+    {
+
     }
 }
