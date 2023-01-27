bb2f526620eac1c4c0984a24c3a8b61c6ffca749
CoolGuyGames <51512210+CoolGuyGames@users.noreply.github.com>
2023-01-26 19:57:08 -0500

    Fixed house. Added NPC. Added quest box

--- Assets/scripts/QuestManager.cs
+++ Assets/scripts/QuestManager.cs
@@ -11,11 +11,21 @@ public class QuestManager : MonoBehaviour
     public Text questText;
     public GameObject questBox;
 
+    public GameObject expandedQuestBox;
+    public GameObject questImage;
+
     public int currentStep;
-
     public bool hasQuest = false;
-
     public int acceptedQuest = 0;
+
+    public Animator questAnim;
+    public AnimationClip expandClip;
+    public Text expandedQuestTitle;
+    public Text expandedQuestAuthor;
+    public Text expandedQuestList;
+
+    private bool isExpanded;
+    private bool canExpand = true;
 
     private void Start()
     {    }
@@ -26,12 +36,31 @@ public class QuestManager : MonoBehaviour
         {
             questText.text = currentQuest + ": " + objectives[currentStep];
             questBox.SetActive(true);
+
+            expandedQuestTitle.text = currentQuest;
+
+            if(expandedQuestList.text == "")
+            {
+                for (int i = 1; i <= objectives.Length; i++)
+                {
+                    expandedQuestList.text += i + ". " + objectives[i - 1] + "\n";
+                }
+            }
         }
         else
         {
             questText.text = "";
             questBox.SetActive(false);
-        }    
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Q) && canExpand)
+        {
+            if (!isExpanded)
+                StartCoroutine("expandAnim");
+            else
+                StartCoroutine("unexpandAnim");
+
+        }
     }
 
     public void Accept()
@@ -41,5 +70,29 @@ public class QuestManager : MonoBehaviour
     public void Decline()
     {
         acceptedQuest = -1;
+    }
+
+    IEnumerator expandAnim()
+    {
+        canExpand = false;
+        questAnim.SetTrigger("expand");
+        questText.gameObject.SetActive(false);
+        yield return new WaitForSecondsRealtime(expandClip.length);
+        questImage.SetActive(false);
+        expandedQuestBox.SetActive(true);
+        isExpanded = true;
+        canExpand = true;
+    }
+
+    IEnumerator unexpandAnim()
+    {
+        canExpand = false;
+        questAnim.SetTrigger("unexpand");
+        questImage.SetActive(true);
+        expandedQuestBox.SetActive(false);
+        yield return new WaitForSecondsRealtime(expandClip.length);
+        questText.gameObject.SetActive(true);
+        isExpanded = false;
+        canExpand = true;
     }
 }
