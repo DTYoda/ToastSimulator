b2c0b09a3434bdfa456a19af19b89b574ce3ff02
CoolGuyGames <51512210+CoolGuyGames@users.noreply.github.com>
2023-03-05 22:09:42 -0500

    Fixed Shop-O-Mart doors

--- Assets/scripts/DoorOpener.cs
+++ Assets/scripts/DoorOpener.cs
@@ -7,31 +7,62 @@ public class DoorOpener : MonoBehaviour
     public bool isOpen = false;
     private float rotateSpeed = 150;
 
+    public bool reverse;
+
     public Camera mainCam;
     private void Update()
     {
-        if (isOpen && transform.localEulerAngles.y < 90)
+        if(!reverse)
         {
-            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+            if (isOpen && transform.localEulerAngles.y < 90)
+            {
+                transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+            }
+            else
+            {
+                if (isOpen)
+                {
+                    transform.localEulerAngles = new Vector3(0, 90, 0);
+                }
+
+            }
+
+            if (!isOpen && !(transform.localEulerAngles.y == 0))
+            {
+                transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+            }
+
+            if (transform.localEulerAngles.y > 350)
+            {
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
         }
         else
         {
-            if (isOpen)
+            if (isOpen && (transform.localEulerAngles.y > 270 || transform.localEulerAngles.y == 0))
             {
-                transform.localEulerAngles = new Vector3(0, 90, 0);
+                transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+            }
+            else
+            {
+                if (isOpen)
+                {
+                    transform.localEulerAngles = new Vector3(0, 270, 0);
+                }
+
             }
 
+            if (!isOpen && !(transform.localEulerAngles.y == 0))
+            {
+                transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+            }
+
+            if (transform.localEulerAngles.y > 4 && transform.localEulerAngles.y < 20)
+            {
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
         }
         
-        if (!isOpen && !(transform.localEulerAngles.y == 0))
-        {
-            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
-        }
-        
-        if (transform.localEulerAngles.y > 350)
-        {
-            transform.localEulerAngles = new Vector3(0, 0, 0);
-        }
             
     }
 }
