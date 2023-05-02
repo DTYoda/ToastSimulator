10860899c059c12d1012783e51b86a6222a28099
CoolGuyGames <51512210+CoolGuyGames@users.noreply.github.com>
2023-05-01 23:07:15 -0400

    Added house Loader

---
+++ Assets/scripts/HouseLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    GameObject houseInsides;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        houseInsides = this.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(this.transform.position, player.transform.position) >= 50)
+        {
+            houseInsides.SetActive(false);
+        }
+        else
+        {
+            houseInsides.SetActive(true);
+        }
+    }
+}
